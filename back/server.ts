81112512dc112e820a9e34e81d9c3541e99b8a4c
liolle <laurentlionel20@hotmail.com>
2023-05-03 13:43:33 +0200

    update server

---
+++ back/server.ts
@@ -0,0 +1,60 @@
+/* Package imports */
+import express from 'express';
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from 'path'
+import dotenv from "dotenv";
+
+
+//ENVIRONNEMENT//
+
+
+let ENV = process.env.ENVIRONNEMENT || ""
+if (ENV == "production"){
+
+    dotenv.config(
+        { 
+            path: path.join(__dirname, '..','.env.production') ,
+            override: true,
+            debug: true
+        }
+    );
+
+}
+else{
+
+    
+    dotenv.config(
+        { 
+            path: path.join(__dirname, '..','.env') ,
+            override: true,
+            debug: true
+        }
+    );
+}
+
+
+const PORT = 3535
+
+const  whitelist = ['https://liolle.github.io','http://localhost:5173','http://localhost:4173']
+
+var corsOptions = {
+  credentials: true,
+  origin: whitelist
+}
+
+//MIDDLEWARE//
+
+const app = express();
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors(corsOptions));
+
+
+//ROUTES//
+
+
+app.listen(PORT,() =>{
+    let ENV = process.env.ENVIRONNEMENT == 'production' ? 'PRODUCTION':'DEVELOPMENT' 
+    console.log( `\nServer running on ---> http://localhost:${PORT} <${ENV}>\n` )
+});
