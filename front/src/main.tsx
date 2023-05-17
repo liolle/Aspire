94ac4ccbcd0255bf1dca834949442d9fcba35a2f
liolle <laurentlionel20@hotmail.com>
2023-05-18 01:57:04 +0200

    modif main and vite config

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import ReactDOM from 'react-dom'
+// import ReactDOM from 'react-dom/client'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App.js'
 import './index.css'
@@ -8,18 +9,27 @@ import './index.css'
 import store from './app/store'
 import { Provider } from 'react-redux'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 
   
 
-  <BrowserRouter>
+//   <HashRouter>
+//      <ChakraProvider>
+//         <Provider store={store}>
+//           <App />
+//         </Provider>
+//     </ChakraProvider>
+//   </HashRouter>,
+// )
+
+ReactDOM.render(
+  <HashRouter>
      <ChakraProvider>
         <Provider store={store}>
           <App />
         </Provider>
     </ChakraProvider>
-  </BrowserRouter>,
+  </HashRouter>,
+  document.getElementById("root")
 )
-
-
 
--- front/vite.config.ts
+++ front/vite.config.ts
@@ -9,6 +9,10 @@ export default defineConfig({
   //     cert: "./apire.crt"
   //   }
   // },
+  base: "./",
   plugins: [react()],
+  build:{
+    emptyOutDir: true,
+  }
 })
 
